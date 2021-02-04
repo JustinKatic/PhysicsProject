7663d7d4bff1d80c30dd9b9c63d1925d3c6e7120
Justin <justinkatic4@gmail.com>
2021-02-05 08:45:07 +1100

    red dot moving

--- aieBootstrap-master/PhysicsProject/PhysicsProjectApp.cpp
+++ aieBootstrap-master/PhysicsProject/PhysicsProjectApp.cpp
@@ -4,6 +4,7 @@
 #include "Input.h"
 #include "glm\ext.hpp"
 #include <Gizmos.h>
+#include "Sphere.h"
 
 
 PhysicsProjectApp::PhysicsProjectApp() {
@@ -16,6 +17,7 @@ PhysicsProjectApp::~PhysicsProjectApp() {
 
 bool PhysicsProjectApp::startup()
 {
+	//increases the 2D line count to maximise the number of objects we an draw
 	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
 
 	m_2dRenderer = new aie::Renderer2D();
@@ -23,8 +25,22 @@ bool PhysicsProjectApp::startup()
 	// TODO: remember to change this when redistributing a build!
 	// the following path would be used instead: "./font/consolas.ttf"
 	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
-	while (true);
-	return true;
+
+	m_physicsScene = new PhysicsScene();
+
+	m_physicsScene->SetGravity(glm::vec2(0, 0));
+
+
+	//lower the value, the more accurate the simulation will be
+	// but it will increase the processing time required.
+	//If it is to high it causes the sim to stutter and reduce stability
+	m_physicsScene->SetTimeStep(0.01f);
+
+	Sphere* ball;
+	ball = new Sphere(glm::vec2(-40, 0), glm::vec2(10, 30), 3.0f, 1, glm::vec4(1, 0, 0, 1));
+	m_physicsScene->AddActor(ball);
+
+		return true;
 }
 
 void PhysicsProjectApp::shutdown() {
@@ -38,12 +54,17 @@ void PhysicsProjectApp::update(float deltaTime) {
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
+	aie::Gizmos::clear();
+	m_physicsScene->Update(deltaTime);
+	m_physicsScene->Draw();
+
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
 }
 
-void PhysicsProjectApp::draw() {
+void PhysicsProjectApp::draw() 
+{
 
 	// wipe the screen to the background colour
 	clearScreen();
