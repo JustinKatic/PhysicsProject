29c84a07adcb90ad538b5b141cd8c5088bc7c6e1
Justin <justinkatic4@gmail.com>
2021-02-17 12:43:48 +1100

    moved mem variables into list

--- aieBootstrap-master/PhysicsProject/PhysicsProjectApp.cpp
+++ aieBootstrap-master/PhysicsProject/PhysicsProjectApp.cpp
@@ -25,25 +25,10 @@ bool PhysicsProjectApp::startup()
 	//increases the 2D line count to maximise the number of objects we an draw
 	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
 
+	CreateTextures();
 
 	m_tableTexture = new aie::Texture("./textures/table.png");
 
-	m_whiteBallTexture = new aie::Texture("./textures/whiteBall.png");
-	m_ball1Texture = new aie::Texture("./textures/ball1.png");
-	m_ball2Texture = new aie::Texture("./textures/ball2.png");
-	m_ball3Texture = new aie::Texture("./textures/ball3.png");
-	m_ball4Texture = new aie::Texture("./textures/ball4.png");
-	m_ball5Texture = new aie::Texture("./textures/ball5.png");
-	m_ball6Texture = new aie::Texture("./textures/ball6.png");
-	m_ball7Texture = new aie::Texture("./textures/ball7.png");
-	m_ball8Texture = new aie::Texture("./textures/ball8.png");
-	m_ball9Texture = new aie::Texture("./textures/ball9.png");
-	m_ball10Texture = new aie::Texture("./textures/ball10.png");
-	m_ball11Texture = new aie::Texture("./textures/ball11.png");
-	m_ball12Texture = new aie::Texture("./textures/ball12.png");
-	m_ball13Texture = new aie::Texture("./textures/ball13.png");
-	m_ball14Texture = new aie::Texture("./textures/ball14.png");
-	m_ball15Texture = new aie::Texture("./textures/ball15.png");
 
 
 
@@ -75,12 +60,9 @@ bool PhysicsProjectApp::startup()
 
 void PhysicsProjectApp::shutdown()
 {
-
 	delete m_font;
 	delete m_2dRenderer;
 	delete m_tableTexture;
-	delete m_ball1Texture;
-
 }
 
 void PhysicsProjectApp::update(float deltaTime)
@@ -122,24 +104,7 @@ void PhysicsProjectApp::draw()
 	// draw your stuff here!
 	m_2dRenderer->drawSprite(m_tableTexture, getWindowWidth() / 2, getWindowHeight() / 2, getWindowWidth(), getWindowHeight(), 0);
 
-	m_2dRenderer->drawSprite(m_whiteBallTexture, whiteBall->GetPosition().x, whiteBall->GetPosition().y, whiteBall->GetRadius() * 2, whiteBall->GetRadius() * 2);
-
-	m_2dRenderer->setUVRect(0, 0, 1, 1);
-	m_2dRenderer->drawSprite(m_ball1Texture, ball1->GetPosition().x, ball1->GetPosition().y, ball1->GetRadius() * 2, ball1->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball2Texture, ball2->GetPosition().x, ball2->GetPosition().y, ball2->GetRadius() * 2, ball2->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball3Texture, ball3->GetPosition().x, ball3->GetPosition().y, ball3->GetRadius() * 2, ball3->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball4Texture, ball4->GetPosition().x, ball4->GetPosition().y, ball4->GetRadius() * 2, ball4->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball5Texture, ball5->GetPosition().x, ball5->GetPosition().y, ball5->GetRadius() * 2, ball5->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball6Texture, ball6->GetPosition().x, ball6->GetPosition().y, ball6->GetRadius() * 2, ball6->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball7Texture, ball7->GetPosition().x, ball7->GetPosition().y, ball7->GetRadius() * 2, ball7->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball8Texture, ball8->GetPosition().x, ball8->GetPosition().y, ball8->GetRadius() * 2, ball8->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball9Texture, ball9->GetPosition().x, ball9->GetPosition().y, ball9->GetRadius() * 2, ball9->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball10Texture, ball10->GetPosition().x, ball10->GetPosition().y, ball10->GetRadius() * 2, ball10->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball11Texture, ball11->GetPosition().x, ball11->GetPosition().y, ball11->GetRadius() * 2, ball11->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball12Texture, ball12->GetPosition().x, ball12->GetPosition().y, ball12->GetRadius() * 2, ball12->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball13Texture, ball13->GetPosition().x, ball13->GetPosition().y, ball13->GetRadius() * 2, ball13->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball14Texture, ball14->GetPosition().x, ball14->GetPosition().y, ball14->GetRadius() * 2, ball14->GetRadius() * 2);
-	m_2dRenderer->drawSprite(m_ball15Texture, ball15->GetPosition().x, ball15->GetPosition().y, ball15->GetRadius() * 2, ball15->GetRadius() * 2);
+	DrawBallTextures();
 
 	// output some text, uses the last used colour
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
@@ -158,11 +123,11 @@ void PhysicsProjectApp::ChooseWhiteBallLocation(aie::Input* a_input)
 	{
 		worldPos.x = a_input->getMouseX();
 		worldPos.y = a_input->getMouseY();
-		whiteBall->SetPosition(glm::vec2(333, worldPos.y));
-		if (whiteBall->GetPosition().y >= 620)
-			whiteBall->SetPosition(glm::vec2(333, 620));
-		else if (whiteBall->GetPosition().y <= 80)
-			whiteBall->SetPosition(glm::vec2(333, 80));
+		m_ListOfBalls[0]->SetPosition(glm::vec2(333, worldPos.y));
+		if (m_ListOfBalls[0]->GetPosition().y >= 620)
+			m_ListOfBalls[0]->SetPosition(glm::vec2(333, 620));
+		else if (m_ListOfBalls[0]->GetPosition().y <= 80)
+			m_ListOfBalls[0]->SetPosition(glm::vec2(333, 80));
 	}
 }
 
@@ -170,7 +135,7 @@ void PhysicsProjectApp::ShootWhiteBall(aie::Input* a_input)
 {
 	worldPos.x = a_input->getMouseX();
 	worldPos.y = a_input->getMouseY();
-	glm::vec2 dist = whiteBall->GetPosition() - worldPos;
+	glm::vec2 dist = m_ListOfBalls[0]->GetPosition() - worldPos;
 	if (glm::length(dist) > 200)
 	{
 		dist = glm::normalize(dist) * 200.0f;
@@ -180,11 +145,11 @@ void PhysicsProjectApp::ShootWhiteBall(aie::Input* a_input)
 	{
 		IsWhiteBallFirstShot = false;
 
-		aie::Gizmos::add2DLine(whiteBall->GetPosition(), whiteBall->GetPosition() - dist, glm::vec4(1, 1, 1, 1));
+		aie::Gizmos::add2DLine(m_ListOfBalls[0]->GetPosition(), m_ListOfBalls[0]->GetPosition() - dist, glm::vec4(1, 1, 1, 1));
 	}
 	if (a_input->wasMouseButtonReleased(0))
 	{
-		whiteBall->ApplyForce(dist * extraForce, glm::vec2(0));
+		m_ListOfBalls[0]->ApplyForce(dist * extraForce, glm::vec2(0));
 	}
 }
 
@@ -196,6 +161,16 @@ void PhysicsProjectApp::AddBallsToBallList(Sphere* a_ball)
 void PhysicsProjectApp::AddPocketsToPocketList(Sphere* a_pocket)
 {
 	m_ListOfPockets.push_back(a_pocket);
+}
+
+void PhysicsProjectApp::AddTexturesToList(aie::Texture* a_texture)
+{
+	m_ListOfTextures.push_back(a_texture);
+}
+
+void PhysicsProjectApp::AddBoxBoardersToList(Box* a_box)
+{
+	m_listOfBoxes.push_back(a_box);
 }
 
 bool PhysicsProjectApp::IsBallVelocity0()
@@ -216,47 +191,47 @@ bool PhysicsProjectApp::IsBallVelocity0()
 
 void PhysicsProjectApp::Createballs()
 {
-	whiteBall = new Sphere(glm::vec2(900, 353), glm::vec2(0, 0), 3.0f, 20, glm::vec4(1, 1, 1, 1));
+	Sphere* whiteBall = new Sphere(glm::vec2(900, 353), glm::vec2(0, 0), 3.0f, 20, glm::vec4(1, 1, 1, 1));
 	m_physicsScene->AddActor(whiteBall);
 
 	//frontRow
-	ball1 = new Sphere(glm::vec2(900, 353), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball1 = new Sphere(glm::vec2(900, 353), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball1);
 
 	//secondRow
-	ball2 = new Sphere(glm::vec2(935, 373), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball2 = new Sphere(glm::vec2(935, 373), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball2);
-	ball3 = new Sphere(glm::vec2(935, 330), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball3 = new Sphere(glm::vec2(935, 330), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball3);
 
 	//thirdRow
-	ball4 = new Sphere(glm::vec2(970, 393), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball4 = new Sphere(glm::vec2(970, 393), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball4);
-	ball5 = new Sphere(glm::vec2(970, 350), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball5 = new Sphere(glm::vec2(970, 350), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball5);
-	ball6 = new Sphere(glm::vec2(970, 307), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball6 = new Sphere(glm::vec2(970, 307), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball6);
 
 	//fourthRow
-	ball7 = new Sphere(glm::vec2(1005, 413), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball7 = new Sphere(glm::vec2(1005, 413), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball7);
-	ball8 = new Sphere(glm::vec2(1005, 370), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball8 = new Sphere(glm::vec2(1005, 370), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball8);
-	ball9 = new Sphere(glm::vec2(1005, 327), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball9 = new Sphere(glm::vec2(1005, 327), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball9);
-	ball10 = new Sphere(glm::vec2(1005, 284), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball10 = new Sphere(glm::vec2(1005, 284), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball10);
 
 	//fifthRow
-	ball11 = new Sphere(glm::vec2(1040, 433), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball11 = new Sphere(glm::vec2(1040, 433), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball11);
-	ball12 = new Sphere(glm::vec2(1040, 390), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball12 = new Sphere(glm::vec2(1040, 390), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball12);
-	ball13 = new Sphere(glm::vec2(1040, 347), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball13 = new Sphere(glm::vec2(1040, 347), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball13);
-	ball14 = new Sphere(glm::vec2(1040, 304), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball14 = new Sphere(glm::vec2(1040, 304), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball14);
-	ball15 = new Sphere(glm::vec2(1040, 261), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
+	Sphere* ball15 = new Sphere(glm::vec2(1040, 261), glm::vec2(0, 0), 3.0f, 20, glm::vec4(0, 1, 1, 1));
 	m_physicsScene->AddActor(ball15);
 
 	AddBallsToBallList(whiteBall);
@@ -279,12 +254,12 @@ void PhysicsProjectApp::Createballs()
 
 void PhysicsProjectApp::CreatePockets()
 {
-	pocket1 = new Sphere(glm::vec2(645, 35), glm::vec2(0, 0), 10, 45, glm::vec4(0, 0, 0, 1));
-	pocket2 = new Sphere(glm::vec2(645, 685), glm::vec2(0, 0), 10, 45, glm::vec4(0, 0, 0, 1));
-	pocket3 = new Sphere(glm::vec2(55, 670), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
-	pocket4 = new Sphere(glm::vec2(1230, 670), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
-	pocket5 = new Sphere(glm::vec2(1230, 55), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
-	pocket6 = new Sphere(glm::vec2(55, 55), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket1 = new Sphere(glm::vec2(645, 35), glm::vec2(0, 0), 10, 45, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket2 = new Sphere(glm::vec2(645, 685), glm::vec2(0, 0), 10, 45, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket3 = new Sphere(glm::vec2(55, 670), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket4 = new Sphere(glm::vec2(1230, 670), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket5 = new Sphere(glm::vec2(1230, 55), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
+	Sphere* pocket6 = new Sphere(glm::vec2(55, 55), glm::vec2(0, 0), 10, 55, glm::vec4(0, 0, 0, 1));
 
 	AddPocketsToPocketList(pocket1);
 	AddPocketsToPocketList(pocket2);
@@ -300,9 +275,9 @@ void PhysicsProjectApp::CreatePockets()
 		m_physicsScene->AddActor(pocket);
 		pocket->m_triggerEnter = [=](PhysicsObject* other)
 		{
-			if (other == whiteBall)
+			if (other == m_ListOfBalls[0])
 			{
-				whiteBall->SetVelocity(glm::vec2(0, 0));
+				m_ListOfBalls[0]->SetVelocity(glm::vec2(0, 0));
 				IsWhiteBallFirstShot = true;
 			}
 
@@ -322,29 +297,80 @@ void PhysicsProjectApp::CreatePockets()
 void PhysicsProjectApp::CreateBoxBoarders()
 {
 	//Bottom pool table border
-	box1 = new Box(glm::vec2(360, 40), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
+	Box* box1 = new Box(glm::vec2(360, 40), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box1);
 	box1->SetKinematic(true);
-	box2 = new Box(glm::vec2(930, 40), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
+	Box* box2 = new Box(glm::vec2(930, 40), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box2);
 	box2->SetKinematic(true);
 	//Top pool table border
-	box3 = new Box(glm::vec2(360, 680), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
+	Box* box3 = new Box(glm::vec2(360, 680), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box3);
 	box3->SetKinematic(true);
-	box4 = new Box(glm::vec2(930, 680), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
+	Box* box4 = new Box(glm::vec2(930, 680), glm::vec2(0, 0), 0, 10, 240, 40, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box4);
 	box4->SetKinematic(true);
 	//Left pool table border
-	box5 = new Box(glm::vec2(40, 365), glm::vec2(0, 0), 0, 10, 40, 240, glm::vec4(0.6, .3, .3, 1));
+	Box* box5 = new Box(glm::vec2(40, 365), glm::vec2(0, 0), 0, 10, 40, 240, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box5);
 	box5->SetKinematic(true);
 	//Right pool table border
-	box6 = new Box(glm::vec2(1240, 365), glm::vec2(0, 0), 0, 10, 40, 240, glm::vec4(0.6, .3, .3, 1));
+	Box* box6 = new Box(glm::vec2(1240, 365), glm::vec2(0, 0), 0, 10, 40, 240, glm::vec4(0.6, .3, .3, 1));
 	m_physicsScene->AddActor(box6);
 	box6->SetKinematic(true);
+
+	AddBoxBoardersToList(box1);
+	AddBoxBoardersToList(box2);
+	AddBoxBoardersToList(box3);
+	AddBoxBoardersToList(box4);
+	AddBoxBoardersToList(box5);
+	AddBoxBoardersToList(box6);
 }
 
+void PhysicsProjectApp::CreateTextures()
+{
+	aie::Texture* m_whiteBallTexture = new aie::Texture("./textures/whiteBall.png");
+	aie::Texture* m_ball1Texture = new aie::Texture("./textures/ball1.png");
+	aie::Texture* m_ball2Texture = new aie::Texture("./textures/ball2.png");
+	aie::Texture* m_ball3Texture = new aie::Texture("./textures/ball3.png");
+	aie::Texture* m_ball4Texture = new aie::Texture("./textures/ball4.png");
+	aie::Texture* m_ball5Texture = new aie::Texture("./textures/ball5.png");
+	aie::Texture* m_ball6Texture = new aie::Texture("./textures/ball6.png");
+	aie::Texture* m_ball7Texture = new aie::Texture("./textures/ball7.png");
+	aie::Texture* m_ball8Texture = new aie::Texture("./textures/ball8.png");
+	aie::Texture* m_ball9Texture = new aie::Texture("./textures/ball9.png");
+	aie::Texture* m_ball10Texture = new aie::Texture("./textures/ball10.png");
+	aie::Texture* m_ball11Texture = new aie::Texture("./textures/ball11.png");
+	aie::Texture* m_ball12Texture = new aie::Texture("./textures/ball12.png");
+	aie::Texture* m_ball13Texture = new aie::Texture("./textures/ball13.png");
+	aie::Texture* m_ball14Texture = new aie::Texture("./textures/ball14.png");
+	aie::Texture* m_ball15Texture = new aie::Texture("./textures/ball15.png");
+
+	AddTexturesToList(m_whiteBallTexture);
+	AddTexturesToList(m_ball1Texture);
+	AddTexturesToList(m_ball2Texture);
+	AddTexturesToList(m_ball3Texture);
+	AddTexturesToList(m_ball4Texture);
+	AddTexturesToList(m_ball5Texture);
+	AddTexturesToList(m_ball6Texture);
+	AddTexturesToList(m_ball7Texture);
+	AddTexturesToList(m_ball8Texture);
+	AddTexturesToList(m_ball9Texture);
+	AddTexturesToList(m_ball10Texture);
+	AddTexturesToList(m_ball11Texture);
+	AddTexturesToList(m_ball12Texture);
+	AddTexturesToList(m_ball13Texture);
+	AddTexturesToList(m_ball14Texture);
+	AddTexturesToList(m_ball15Texture);
+}
+
+void PhysicsProjectApp::DrawBallTextures()
+{
+	for (int i = 0; i < m_ListOfBalls.size(); i++)
+	{
+		m_2dRenderer->drawSprite(m_ListOfTextures[i], m_ListOfBalls[i]->GetPosition().x, m_ListOfBalls[i]->GetPosition().y, m_ListOfBalls[i]->GetRadius() * 2, m_ListOfBalls[i]->GetRadius() * 2);
+	}
+}
 
 //glm::vec2 PhysicsProjectApp::ScreenToWorld(glm::vec2 a_screenPos)
 //{
@@ -357,6 +383,7 @@ void PhysicsProjectApp::CreateBoxBoarders()
 //	return worldPos;
 //}
 
+/*
 void PhysicsProjectApp::DrawRect()
 {
 	m_physicsScene->AddActor(new Sphere(glm::vec2(20, 10), glm::vec2(-10, -17), 1, 3, glm::vec4(1, 0, 0, 1)));
@@ -436,5 +463,7 @@ void PhysicsProjectApp::TriggerTest()
 	ball2->m_triggerEnter = [=](PhysicsObject* other) {ball2->SetPosition(glm::vec2(-90, 50)); };
 	ball2->m_triggerExit = [=](PhysicsObject* other) {std::cout << "EXITED: " << other << std::endl; };
 }
+
+*/
 
 
--- aieBootstrap-master/PhysicsProject/PhysicsProjectApp.h
+++ aieBootstrap-master/PhysicsProject/PhysicsProjectApp.h
@@ -23,8 +23,6 @@ public:
 	virtual void update(float deltaTime);
 	virtual void draw();
 
-	//glm::vec2 ScreenToWorld(glm::vec2 a_screenPos);
-
 protected:
 	glm::vec2 worldPos;
 
@@ -36,10 +34,7 @@ protected:
 
 	PhysicsScene* m_physicsScene;
 
-	//const float m_aspectRatio = 16.0f / 9.0f;
-	//const float m_extents = 100;
 
-	aie::Texture* m_tableTexture;
 
 	void ChooseWhiteBallLocation(aie::Input* a_input);
 	void ShootWhiteBall(aie::Input* a_input);
@@ -52,76 +47,30 @@ protected:
 	void AddPocketsToPocketList(Sphere* a_pocket);
 	std::vector<Sphere*> m_ListOfPockets;
 
+	void AddTexturesToList(aie::Texture* a_texture);
+	std::vector<aie::Texture*> m_ListOfTextures;
+
+	void AddBoxBoardersToList(Box* a_box);
+	std::vector<Box*> m_listOfBoxes;
+
 	bool IsBallVelocity0();
 
 	void Createballs();
 
 	void CreatePockets();
 
-
 	void CreateBoxBoarders();
 
+	void CreateTextures();
 
-	aie::Texture* m_testTexture;
+	void DrawBallTextures();
 
-
-	aie::Texture* m_whiteBallTexture;
-
-
-	aie::Texture* m_ball1Texture;
-	aie::Texture* m_ball2Texture;
-	aie::Texture* m_ball3Texture;
-	aie::Texture* m_ball4Texture;
-	aie::Texture* m_ball5Texture;
-	aie::Texture* m_ball6Texture;
-	aie::Texture* m_ball7Texture;
-	aie::Texture* m_ball8Texture;
-	aie::Texture* m_ball9Texture;
-	aie::Texture* m_ball10Texture;
-	aie::Texture* m_ball11Texture;
-	aie::Texture* m_ball12Texture;
-	aie::Texture* m_ball13Texture;
-	aie::Texture* m_ball14Texture;
-	aie::Texture* m_ball15Texture;
-
-
-	Sphere* whiteBall;
-	Sphere* ball1;
-	Sphere* ball2;
-	Sphere* ball3;
-	Sphere* ball4;
-	Sphere* ball5;
-	Sphere* ball6;
-	Sphere* ball7;
-	Sphere* ball8;
-	Sphere* ball9;
-	Sphere* ball10;
-	Sphere* ball11;
-	Sphere* ball12;
-	Sphere* ball13;
-	Sphere* ball14;
-	Sphere* ball15;
-
-	Box* box1;
-	Box* box2;
-	Box* box3;
-	Box* box4;
-	Box* box5;
-	Box* box6;
-
-	Sphere* pocket1;
-	Sphere* pocket2;
-	Sphere* pocket3;
-	Sphere* pocket4;
-	Sphere* pocket5;
-	Sphere* pocket6;
-
-
+	aie::Texture* m_tableTexture;
 
 public:
 	void DrawRect();
-	void SphereAndPlane();
-	void SpringTest(int a_amount);
-	void TriggerTest();
+	//void SphereAndPlane();
+	//void SpringTest(int a_amount);
+	//void TriggerTest();
 
 };
